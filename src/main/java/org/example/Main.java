92d61aaf0b6abe1055f25057357887f014622c6c
eholmes83 <eholmes83@yahoo.com>
2023-06-26 00:05:16 -0400

    creation of CSV reader object to run app

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.example.service.CSVReader;
+
+import java.nio.file.Path;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        CSVReader csvReader = CSVReader.getInstance();
+        csvReader.readProductsFromFile(Path.of("product-inventory.txt"));
+
     }
 }
